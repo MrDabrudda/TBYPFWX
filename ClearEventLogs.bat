62b80b89ea34fec5e236a8fd51823a2d5a8cf98d
MrDabrudda <39137310+MrDabrudda@users.noreply.github.com>
2018-05-16 08:00:47 -0600

    Add files via upload

---
+++ ClearEventLogs.bat
@@ -0,0 +1,7 @@
+@echo on
+setlocal
+for /f "tokens=1*" %%i in ('wevtutil el') do (
+	if /i not "%%i"=="System" if "%%j"=="" (wevtutil cl %%i) else wevtutil cl "%%i %%j"
+)
+wevtutil cl System
+endlocal
